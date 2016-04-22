9051496e88996bbe40718752926f08b9449944a3
rexymarten <rexymarten.stud@gmail.com>
2016-04-23 00:55:38 +0800

    155150201111330_REXY MARTEN

--- MainTamoe.java
+++ MainTamoe.java
@@ -1,13 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Rexy
- */
+import java.util.Scanner;
 public class MainTamoe {
+    private static Object[] bukutamu;
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+        System.out.println("Masukkan Jumlah Maksimal Tamu\t: ");
+        int jmlh = scn.nextInt();
+        bukutamu1 buku[] = new bukutamu1[jmlh];
+int banyak = 0;        
+    do{
+        System.out.println("-----------GUEST BOOK DIES NATALIS FILKOM-----------");
+        System.out.println("Anda mendapat nomor ID\t:"+bukutamu1.idTamu());
+        
+        System.out.println("Nama Lengkap\t\t:");
+        String n = in.next();
+        System.out.println("Alamat Rumah / Kos\t:");
+        String m = in.next();
+        System.out.println("Nomor Telepon / Handphone\t:");
+        String o = in.next();
+        System.out.println("E-mail\t\t:");
+        String p = in.next();
+        bukutamu1[] tamu = null;
+        
+        tamu[banyak] = new bukutamu1(n,m,o,p);
+        banyak++;
+        
+    }
+    while (banyak<jmlh);{
+        for(int a=0;a<banyak;a++);
+          
+        bukutamu[a].Display();
+    }
+        System.out.println("=========================");
+    }
     
-}
+
--- bukutamu1.java
+++ bukutamu1.java
@@ -9,5 +9,43 @@
  * @author Rexy
  */
 public class bukutamu1 {
-    
+
+     private static String nama,alamat,email;
+    private final String telpon;
+    private static int id = 1;
+    private static int jmlhtamu  = 0;
+    public bukutamu1(String nm,String al,String em,String tel){
+        nama = nm;
+        alamat = al;
+        telpon = tel;
+        email = em;
+        jmlhtamu++;
+        
 }
+        public final String aksesNama(){
+            return nama;
+        }
+public final String aksesAlamat(){
+            return alamat;
+}
+public final String aksesEmail(){
+            return email;
+}
+public final String aksesTelpon(){
+            return telpon;
+}
+
+    public void Display(){
+         System.out.println("Nama           : " +nama);
+         System.out.println("Alamat         : " +alamat);
+         System.out.println("Nomor Telepon  : "+ telpon);
+         System.out.println("E-mail         :" + email);
+    }
+public static int idTamu(){
+    return id;
+}
+
+}
+
+
+
